22e73c6924c7316aa5706cc22fd6e14bc8c1758f
Diptonil <diptonilr@gmail.com>
2021-06-22 15:44:38 +0530

    Rework(III): basicsII.java

--- app/src/main/java/com/example/aksharas/basicsII.java
+++ app/src/main/java/com/example/aksharas/basicsII.java
@@ -21,15 +21,24 @@ public class basicsII extends AppCompatActivity
 {
     Button b1, b2 , b3;
     ImageButton br1, br2, br3;
-    MediaPlayer boy, girl, fruit;
     boolean l = true, r = false;
-
+    MediaPlayer boy, girl, fruit;
+    boolean ans1 = false, ans2 = false, ans3 = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basics2);
+        b1 = (Button)findViewById(R.id.button6);
+        b2 = (Button)findViewById(R.id.button);
+        b3 = (Button)findViewById(R.id.button10);
+        br1 = (ImageButton)findViewById(R.id.imageButton);
+        br2 = (ImageButton)findViewById(R.id.imageButton2);
+        br3 = (ImageButton)findViewById(R.id.imageButton3);
+        b1.setOnClickListener(v -> but1());
+        b2.setOnClickListener(v -> but2());
+        b3.setOnClickListener(v -> but3());
 
         boy = MediaPlayer.create(basicsII.this, R.raw.boy);
         final MediaPlayer agni = MediaPlayer.create(this, R.raw.boy);
@@ -40,17 +49,9 @@ public class basicsII extends AppCompatActivity
         fruit = MediaPlayer.create(basicsII.this, R.raw.fruit);
         final MediaPlayer arti = MediaPlayer.create(this, R.raw.fruit);
 
-        b1 = (Button)findViewById(R.id.button6);
-        b2 = (Button)findViewById(R.id.button);
-        b3 = (Button)findViewById(R.id.button10);
-
-        br1 = (ImageButton)findViewById(R.id.imageButton);
-        br2 = (ImageButton)findViewById(R.id.imageButton2);
-        br3 = (ImageButton)findViewById(R.id.imageButton3);
-
-        b1.setOnClickListener(v -> but1());
-        b2.setOnClickListener(v -> but2());
-        b3.setOnClickListener(v -> but3());
+        br1.setOnClickListener(v -> butt1());
+        br2.setOnClickListener(v -> butt2());
+        br3.setOnClickListener(v -> butt3());
 
         b1.setOnClickListener(v -> Playfruit());
         b2.setOnClickListener(v -> Playboy());
@@ -62,6 +63,21 @@ public class basicsII extends AppCompatActivity
 
     public void but1()
     {
+        if(ans1)
+        {
+            b2.setEnabled(false);
+            br1.setEnabled(false);
+        }
+        if(ans2)
+        {
+            b3.setEnabled(false);
+            br2.setEnabled(false);
+        }
+        if(ans3)
+        {
+            b1.setEnabled(false);
+            br3.setEnabled(false);
+        }
         if (l == true)
         {
             b1.setEnabled(false);
@@ -73,6 +89,21 @@ public class basicsII extends AppCompatActivity
 
     public void but2()
     {
+        if(ans1)
+        {
+            b2.setEnabled(false);
+            br1.setEnabled(false);
+        }
+        if(ans2)
+        {
+            b3.setEnabled(false);
+            br2.setEnabled(false);
+        }
+        if(ans3)
+        {
+            b1.setEnabled(false);
+            br3.setEnabled(false);
+        }
         if (l == true)
         {
             b1.setEnabled(false);
@@ -85,14 +116,131 @@ public class basicsII extends AppCompatActivity
 
     public void but3()
     {
-        if (l == true)
-        {
+        if (ans1) {
+            b2.setEnabled(false);
+            br1.setEnabled(false);
+        }
+        if (ans2) {
+            b3.setEnabled(false);
+            br2.setEnabled(false);
+        }
+        if (ans3) {
+            b1.setEnabled(false);
+            br3.setEnabled(false);
+        }
+        if (l == true) {
             b1.setEnabled(false);
             l = false;
             r = true;
         }
-
     }
+
+        public void butt1()
+        {
+            if(ans1)
+            {
+                b2.setEnabled(false);
+                br1.setEnabled(false);
+            }
+            if(ans2)
+            {
+                b3.setEnabled(false);
+                br2.setEnabled(false);
+            }
+            if(ans3)
+            {
+                b1.setEnabled(false);
+                br3.setEnabled(false);
+            }
+            if(r == true)
+            {
+                br1.setEnabled(false);
+                l = true;
+                r = false;
+            }
+            if(!(b2.isEnabled()))
+            {
+                ans1 = true;
+            }
+            else
+            {
+                b3.setEnabled(true);
+                b1.setEnabled(true);
+                br1.setEnabled(true);
+            }
+        }
+
+
+        public void butt2()
+        {
+            if(ans1)
+            {
+                b2.setEnabled(false);
+                br1.setEnabled(false);
+            }
+            if(ans2)
+            {
+                b3.setEnabled(false);
+                br2.setEnabled(false);
+            }
+            if(ans3)
+            {
+                b1.setEnabled(false);
+                br3.setEnabled(false);
+            }
+            if(r == true)
+            {
+                br2.setEnabled(false);
+                l = true;
+                r = false;
+            }
+            if(!(b3.isEnabled()))
+            {
+                ans2 = true;
+            }
+            else
+            {
+                b1.setEnabled(true);
+                b2.setEnabled(true);
+                br2.setEnabled(true);
+            }
+        }
+
+
+        public void butt3()
+        {
+            if(ans1)
+            {
+                b2.setEnabled(false);
+                br1.setEnabled(false);
+            }
+            if(ans2)
+            {
+                b3.setEnabled(false);
+                br2.setEnabled(false);
+            }
+            if(ans3)
+            {
+                b1.setEnabled(false);
+                br3.setEnabled(false);
+            }
+            if(r == true)
+            {
+                br3.setEnabled(false);
+                l = true;
+                r = false;
+            }
+            if(!(b1.isEnabled()))
+            {
+                ans3 = true;
+            }
+            else
+            {
+                b2.setEnabled(true);
+                b3.setEnabled(true);
+                br3.setEnabled(true);
+            }
+        }
     public void Playboy(){ boy.start();
     }
     public void Playgirl(){ girl.start();
@@ -100,4 +248,5 @@ public class basicsII extends AppCompatActivity
 
     public void Playfruit(){ fruit.start();
     }
-}
+
+    }
