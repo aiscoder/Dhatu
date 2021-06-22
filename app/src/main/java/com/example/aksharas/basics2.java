82767b9be4e474f26b9722737059a3a964fc9cff
Ash2277 <78946808+Ash2277@users.noreply.github.com>
2021-06-22 13:54:57 +0530

    Dhatu

---
+++ app/src/main/java/com/example/aksharas/basics2.java
@@ -0,0 +1,14 @@
+package com.example.aksharas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class basics2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basics2);
+    }
+}
